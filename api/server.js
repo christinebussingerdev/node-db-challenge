1459cc24cd92a03a43a649c821c486680a079d88
Christine Bussinger <christinebussingerdev@gmail.com>
2020-05-08 23:25:17 -0400

    api initialized, base dependencies added

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+//import routers
+// const projectRouter = require('./api/routers/projectRouter')
+
+//server init
+const server = express();
+
+server.use(express.json())
+
+// use routers
+// server.use('/projects', projectRouter)
+
+// root msg
+server.get('/', (req, res) => {
+  res.status(200).json('server listening...')
+})
+
+module.exports = server
